fed9b81619fadd03d82a16b39e2ef3b0e54b88e4
Shahid Raza <raza59261@gmail.com>
2025-09-08 12:42:47 +0530

    fix the tailwnd css

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ const rootDir = require('./utils/pathUtils');
 
 const app = express();
 
-app.use((req,res,next)=>{
-  console.log(req.url,req.method);
-  next();
-});
+app.use(express.static(path.join(rootDir,'public')));
 app.use(express.urlencoded());
 
 app.use(userRouter);
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./views/**/*.html",   // scan all HTML files in views
+    "./public/*/*.js"        // (optional) if you add custom JS using Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
